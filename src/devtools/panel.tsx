ecb5bd6dd54effc63606e7e85a0c8bd318602a30
Eduard Jacko <eduardjacko@Eduards-MacBook-Pro.local>
2020-01-11 12:21:24 +0000

    popup now using react

--- src/devtools/panel.tsx
+++ src/devtools/panel.tsx
@@ -2,7 +2,3 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(<p>hi</p>, document.getElementById("root"));
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-  alert("test");
-});
